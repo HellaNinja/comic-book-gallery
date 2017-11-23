7721aae08e6f0792059c6b571df8f062af539ca1
j.kevand <j.kevand@gmail.com>
2017-11-22 22:10:25 -0800

    Minor Controller Inheritance

--- ComicBookGallery/Controllers/ComicBookController.cs
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBookController
+    public class ComicBookController : Controller
     {
     }
 }
